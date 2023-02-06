6e68aea087dd7f57a754eb8b5e8afee2d1e4f60c
tooflesswulf <tooflesswulf0325@gmail.com>
2023-02-06 11:58:16 -0500

    delete webpack config

--- apps/frontend/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { composePlugins, withNx } = require('@nrwl/webpack');
-const { withReact } = require('@nrwl/react');
-
-// Nx plugins for webpack.
-module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // https://github.com/nrwl/nx/issues/13628#issuecomment-1407416656
-  config.output.scriptType = 'text/javascript'
-  return config;
-});
